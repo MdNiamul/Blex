c6f0c2e733fa01fa7d827bc44398f40d193a4d3d
Md Niamul Islam <108489887+MdNiamul@users.noreply.github.com>
2025-12-06 23:14:21 +0600

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+int num1, num2;
+
+num1=50;
+num2=30;
+
+int max = (num1 >num2) ? num1 : num2;
+cout<<max;
+
+
+    getch();
+}
